0ff172414107700f9e6133aac160394702ba28ec
Calvin <calvin.seaphanh@ynov.com>
2024-11-12 12:20:55 +0100

    mesh

--- Génération/Assets/Scripts/Display.cs
+++ Génération/Assets/Scripts/Display.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Display : MonoBehaviour
 {
     public Renderer textureNoise;
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
 
     public void DrawNoiseMap(Texture2D texture)
     {
         textureNoise.sharedMaterial.mainTexture = texture;
         textureNoise.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRenderer.sharedMaterial.mainTexture = texture;
     }
 }
